8586996cc6e758a039aba1b8d97314ec8d452f67
YooSeongYeob <yoostar94@naver.com>
2023-04-19 10:34:40 +0900

    데이터베이스 수정페이지 데이터베이스 입력

--- src/common/db_connect.php
+++ src/common/db_connect.php
@@ -25,5 +25,53 @@ function db_conn( &$param_conn )
         throw new Exception( $e->getMessage() );
     }
 }
+//-----------------------------------------------//
+//------------수정페이지 데이터베이스------------//
+//----------------------------------------------//
+function update_todo_list_info_no( &$param_arr )
+{
+    $sql = 
+    " UPDATE "
+    ." todo_list_info "
+    ." SET "
+    ." list_title = :list_title "
+    ." ,list_detail = :list_detail "
+    ." ,list_start_date = :list_start_date "
+    ." ,list_due_date = :list_due_date "
+    ." ,list_imp_flg = :list_imp_flg "
+    ." WHERE "
+    ." list_no = :list_no "
+    ;
+    $arr_prepare =
+        array(
+            ":list_title" => $param_arr["list_title"]
+            ,":list_detail" => $param_arr["list_detail"]
+            ,":list_start_date" => $param_arr["list_start_date"]
+            ,":list_due_date" => $param_arr["list_due_date"] 
+            ,":list_imp_flg" => $param_arr["list_imp_flg"] 
+            );
+
+    $conn = null;
+    try
+    {
+        db_conn( $conn );
+        $conn->beginTransaction();
+        $stmt = $conn->prepare( $sql );
+        $stmt->execute( $arr_prepare );
+        $result_cnt = $stmt->rowCount();
+        $conn->commit();
+    }
+    catch( Exception $e )
+    {
+        $conn->rollback();
+        return $e->getMessage();
+        
+    }
+    finally
+    {
+        $conn = null; 
+    }
+    return $result_cnt;
+}
 
 ?>
