cd211c0ff517e1af2a5af0aec211a6137506cb02
qsczeadwx1 <qsczeadwx1@gmail.com>
2023-04-20 11:33:54 +0900

    0420 path rollback

--- src/todo_detail.php
+++ src/todo_detail.php
@@ -1,5 +1,5 @@
 <?php 
-    define( "SRC_ROOT", $_SERVER["DOCUMENT_ROOT"]."/PHP_1STPJ/src/" );
+    define( "SRC_ROOT", $_SERVER["DOCUMENT_ROOT"]."/PHP_1STPJ-main/src/" );
     define( "URL_DB", SRC_ROOT."common/db_connect.php" );
     include_once( URL_DB );
     // $arr_prepare = array(
