891285317d3eeef684cef8744c7e55bde1f7a0c7
qsczeadwx1 <qsczeadwx1@gmail.com>
2023-04-20 13:04:02 +0900

    0420 delete_update

--- src/todo_delete.php
+++ src/todo_delete.php
@@ -1,14 +1,18 @@
 <?php
     define( "SRC_ROOT", $_SERVER["DOCUMENT_ROOT"]."/PHP_1STPJ-main/src/" );
-    define( "URL_DB", SRC_ROOT."todo_insert_db.php" );
+    define( "URL_DB", SRC_ROOT."common/db_connect.php" );
     include_once( URL_DB );
 
-    $arr_get = $_GET;
+        $arr_get = $_GET;
+        var_dump($arr_get);
+        $arr_prepare = array(
+            "list_no" => (int)$arr_get["list_no"]
+        );
+        $result_cnt = delete_todo_info( $arr_get["list_no"] );
 
-    $result_info = delete_todo_info( $arr_get["list_no"] );
-
-    // header( "Location: todo_index.php" );
-    // exit;
+    
+            // header( "Location: todo_index.php" );
+        // exit;
     // 취소버튼에 a태그 상세페이지로 가는거 리스트 넘버가져오는 함수 사용해서 넣기
     // 삭제버튼 누르면 list DB에서 삭제하고 리스트페이지로 가도록
 ?>
@@ -29,8 +33,10 @@
         <p>퀘스트를 포기하시겠습니까?</p>
         <p>주의! 포기한 퀘스트는 사라집니다</p>
     </div>
-        <button><a href="todo_detail.php?list_no<?php  ?>">취소</a></button>
+        <button><a href="todo_detail.php?list_no=<?php echo $arr_get["list_no"] ?>">취소</a></button>
         <button type="submit">삭제</button>
+
+        
     </div>
 </body>
 </html>
