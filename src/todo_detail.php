531cbf623962b7367b9776112003bf4696301189
chaesuji <suji0251@naver.com>
2023-04-21 14:54:49 +0900

    0421 detail_error

--- src/todo_detail.php
+++ src/todo_detail.php
@@ -43,43 +43,49 @@
     <link rel="stylesheet" href="./css/todo_detail_c.css">
     <title>Detail</title>
 </head>
+<header>
+    <img src="./common/img/title.png" alt="header_title">
+</header>
 <body>
     <form action="todo_detail.php" method="post">
         <div class="detail">
             <div class="profile"> <!-- 프로필 -->
                 <div class="prof_img">
-                    <!-- <img src="./common/grow1.png" alt="profile"> -->
+                    
                 </div>
                 <span class="prof_name_level">
                     Lv. 이름<br>
                     name
                 </span>
+                <hr class="prof_hr">
             </div>
-            <div class="detail_calender"> <!-- 달력 -->
-                <h3>Calender</h3>
-                <table> <!-- html&css 달력(고정된 값의 달력) -->
-                    <thead>< 23.04 ></thead>
-                    <tr>
-                        <?php for ($i=1; $i<=7; $i++) { ?>
-                            <td><?php echo "○" ?></td>
-                        <?php } ?>
-                    </tr>
-                    <tr>
-                    <?php for ($i=1; $i<=7; $i++) { ?>
-                            <td><?php echo "○" ?></td>
-                        <?php } ?>
-                    </tr>
-                    <tr>
-                    <?php for ($i=1; $i<=7; $i++) { ?>
-                            <td><?php echo "○" ?></td>
-                        <?php } ?>
-                    </tr>
-                    <tr>
-                    <?php for ($i=1; $i<=7; $i++) { ?>
-                            <td><?php echo "○" ?></td>
-                        <?php } ?>
-                    </tr>
-                </table>
+            <div class="detail_total_calender">
+            <h3>Calender</h3>
+                <div class="detail_calender"> <!-- 달력 -->
+                    <table> <!-- html&css 달력(고정된 값의 달력) -->
+                            <p>< 23.04 ></p>
+                            <tr>
+                                <?php for ($i=1; $i<=7; $i++) { ?>
+                                    <td><?php echo " ○ " ?></td>
+                                <?php } ?>
+                            </tr>
+                            <tr>
+                            <?php for ($i=1; $i<=7; $i++) { ?>
+                                    <td><?php echo " ○ " ?></td>
+                                <?php } ?>
+                            </tr>
+                            <tr>
+                            <?php for ($i=1; $i<=7; $i++) { ?>
+                                    <td><?php echo " ○ " ?></td>
+                                <?php } ?>
+                            </tr>
+                            <tr>
+                            <?php for ($i=1; $i<=7; $i++) { ?>
+                                    <td><?php echo " ○ " ?></td>
+                                <?php } ?>
+                            </tr>
+                    </table>
+                </div>
             </div>
             <div class="detail_today"> <!-- 현재 선택한 할 일과 같은 날의 남은 할 일 표시 -->
                 <h3>Left to do</h3>
@@ -94,7 +100,7 @@
                             <?php 
                                 // if(1 == $today_list){ 
                                 ?>
-                                <a href="todo_detail.php?list_no=<?php echo $value["list_no"]?>&list_start_no=<?php echo date("Y-m-d", strtotime($value["list_start_date"])) ?>">
+                                <a class="left_todo" href="todo_detail.php?list_no=<?php echo $value["list_no"]?>&list_start_no=<?php echo date("Y-m-d", strtotime($value["list_start_date"])) ?>">
                                     <?php echo $value["list_title"]." ".date("m.d / H : i", strtotime($value["list_start_date"]))." ~ ".date("H : i", strtotime($value["list_due_date"]));?>
                                 </a>
                             <?php } ?>
@@ -106,38 +112,39 @@
         </div>
         <div class="detail_info"> <!-- 현재 선택한 할 일 제목, 날짜, 상세 내용 -->
             <div class="info_title"> 
-                <h2><?php echo $detail_info["list_title"] ?></h2>
                 <span class="todo_date">
                     <!-- strtotime() : 문자열 형태의 날짜를 입력받아 UNIX timestamp(초 단위로 세어지는 정수로 표현한 값) 형식의 값을 돌려주는 함수 -->
                     <?php echo date("m.d", strtotime($detail_info["list_start_date"]))." ~ ".date("m.d", strtotime($detail_info["list_due_date"])); ?>
-                    <?php if($detail_info["list_imp_flg"] === '1'){ ?>
-                        <span class="imp">★</span>
-                    <?php }else{ ?>
-                        <span class="no_imp">☆</span>
-                    <?php } ?>
                 <span>
                 <hr>
             </div>
-                <div class="detail_content">
-                    <!-- <form action="todo_index.php" method="post" class="detail_title"> -->
-                    <input type="hidden" value="<?= $arr_prepare["list_no"] ?>" name="list_no">
-                    <?php if($detail_info["list_clear_flg"] === '1'){ ?>
-                        <input type="checkbox" value="0" checked>
-                    <?php }else{ ?>
-                        <input type="checkbox" name="check" class="todo_check" value="check">
-                    <?php } ?>
-                    <span class="todo_title"><?= $detail_info["list_title"] ?> <span>
-                    <span class="todo_date">
-                        <?php echo date("H : i", strtotime($detail_info["list_start_date"]))." ~ ".date("H : i", strtotime($detail_info["list_due_date"])); ?>
-                    <span>
-                    <div class="detila_content">
-                        <textarea name="" id="" cols="50" rows="10" readonly>
-                            <?php echo $detail_info["list_detail"]?>
-                        </textarea>
+            <div class="imp_star">
+                <?php if($detail_info["list_imp_flg"] === '1'){ ?>
+                    <span class="imp">★</span>
+                <?php }else{ ?>
+                    <span class="no_imp">☆</span>
+                <?php } ?>
+            </div>
+            <div class="detail_content">
+                <div class="detail_title">
+                    <div class="detail_info_title">
+                        <input type="hidden" value="<?= $arr_prepare["list_no"] ?>" name="list_no">
+                        <?php if($detail_info["list_clear_flg"] === '1'){ ?>
+                            <input type="checkbox" value="0" checked>
+                        <?php }else{ ?>
+                            <input type="checkbox" name="check" class="todo_check" value="check">
+                        <?php } ?>
+                        <span class="todo_title"><?= $detail_info["list_title"] ?> <span>
+                        <span class="todo_date_time">
+                            <?php echo date("H : i", strtotime($detail_info["list_start_date"]))." ~ ".date("H : i", strtotime($detail_info["list_due_date"])); ?>
+                        <span>
                     </div>
                 </div>
+                    <textarea name="" id="" cols="50" rows="10" readonly>
+                        <?php echo $detail_info["list_detail"]?>
+                    </textarea>
+                </div>
                 <button type="submit" class="com">완료</button>
-            <!-- </form> -->
         </div>
         <a href="todo_update.php?list_no=<?php echo $arr_prepare["list_no"] ?>"><button type="button">수정</button></a>
         <a href="todo_index.php"><button type="button">돌아가기</button></a>
