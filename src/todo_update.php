fa4f58796b448c44d1155121b54d00cb20d281d3
YooSeongYeob <yoostar94@naver.com>
2023-04-21 14:56:43 +0900

    todo_update !!

--- src/todo_update.php
+++ src/todo_update.php
@@ -43,7 +43,7 @@ else
       "list_due_date" => $arr_post["list_due_date"],
       "list_imp_flg" => $list_imp_flg
          );
-         
+
     $result_cnt = update_todo_list_info_no( $arr_info );  
 
         
@@ -100,6 +100,10 @@ else
 //       ,"list_due_date" => $arr_post["list_due_date"] 
 //       ,"list_imp_flg" => $list_imp_flg
 //       );
+
+// <div class= "contents_no"> 
+// <label for="list_no">목록 번호</label>
+//   <input type="text" name="list_no" value="<?php echo $result_info["list_no"] ?>" readonly >
 ?>
 
 <!DOCTYPE html>
@@ -112,31 +116,46 @@ else
 <title>수정하기</title>
 </head>
 <body>
-<form method="post" action="todo_update.php">
-  <div class= "list_edit">
-     <label for="list_no">목록 번호</label>
-      <input type="text" name="list_no" value="<?php echo $result_info["list_no"] ?>" readonly >
-      <br>
-      <label for="list_title">제목</label>
-      <input type="text" name="list_title" value="<?php echo $result_info["list_title"] ?>" required>
-      <br>
+<div class="container">
+    <div class="header">
+        <h1><a href="todo_index.php"><img src="common/img/title.png" alt="title"></a></h1>
+    </div>
+  <br>
+    <div class="contents_outside">
+    <form method="post" action="todo_update.php">
+    <div class= "contents_title">
+        <label for="list_title">제목</label>
+        <input type="text" name="list_title" value="<?php echo $result_info["list_title"] ?>" required>
+    </div>
+  <br>
+    <div class= "contents_detail">
       <label for="list_detail">내용</label>
       <input type="text" name="list_detail" id="list_detail" value="<?php echo $result_info["list_detail"] ?>" required>
-      <br>
-      <label for="list_start_date">시작 날짜</label>
-      <input type="datetime-local" name="list_start_date" id="list_start_date" value="<?php echo $result_info["list_start_date"] ?>" required>
-      <br>
-      <label for="list_due_date">마감 날짜</label>
-      <input type="datetime-local" name="list_due_date" id="list_due_date" value="<?php echo$result_info["list_due_date"] ?>" required>
-      <br>
-      <label for="list_imp_flg">중요</label>
-      <input type="checkbox" name="list_imp_flg" id="list_imp_flg" value="1" <?php echo $one_1; ?>>
-      <br>
+    </div>
+  <br>
+    <div class="start_end_date">
+        <span class="start_date_dir">
+          <label for="list_start_date">시작 날짜</label>
+          <input type="datetime-local" name="list_start_date" id="list_start_date" value="<?php echo $result_info["list_start_date"] ?>" required>
+        </span>
+        <span class="end_date_dir">
+          <label for="list_due_date">마감 날짜</label>
+          <input type="datetime-local" name="list_due_date" id="list_due_date" value="<?php echo$result_info["list_due_date"] ?>" required>
+        </span>
+    </div>
+  <br>
+    <div class="contents_imp">
+        <label for="list_imp_flg">중요</label>
+        <input type="checkbox" name="list_imp_flg" id="list_imp_flg" value="1" <?php echo $one_1; ?>>
+    </div>
+    </div>
       <button type="submit">수정</button>
       <a href = "todo_detail.php?list_no=<?php echo $result_info["list_no"]."&list_start_date=".substr($result_info["list_start_date"],0,10) ?>"><button type="button">취소</button></a> 
       <a href = "todo_delete.php?list_no=<?php echo $result_info["list_no"]."&list_start_date=".substr($result_info["list_start_date"],0,10) ?>"><button type="button">삭제</button></a>
-  <div>
+ 
 </form>
+</div>
+</div>
 <!-- <button type="button" ><a href= "list_detail.php" id="hii">목록</a></button> -->
 </body>
 </html>
