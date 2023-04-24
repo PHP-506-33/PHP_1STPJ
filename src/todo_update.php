80be4fab19d1069ce9c6193d4d5f7a72e8bb415e
YooSeongYeob <yoostar94@naver.com>
2023-04-24 11:49:45 +0900

    todo_update 1_project

--- src/todo_update.php
+++ src/todo_update.php
@@ -52,7 +52,7 @@ else
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel='stylesheet' href='../src/css/todo_update_c.css'>
-<title>수정하기</title>
+<title>To do list : 수정</title>
 </head>
 <body>
   <div class= "container">
